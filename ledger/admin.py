14793da9cbbc8b43c823900642154448262623db
tatine <tatineeee@gmail.com>
2026-02-19 19:05:15 +0800

    added admin panel

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Ingredient, Recipe, RecipeIngredient
 
-# Register your models here.
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeIngredientInline]
+    search_fields = ("name",)
+    list_display = ("name",)
+
+
+admin.site.register(Ingredient)
+admin.site.register(Recipe, RecipeAdmin)
