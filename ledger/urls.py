cfa83513ee2df26e230acb3780c5d0e3edc90d2c
tatine <tatineeee@gmail.com>
2026-03-02 20:35:13 +0800

    updated url to use registration html file

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import include, path
 from .views import recipe_list, recipe_detail
 
 urlpatterns = [
     path("recipes/list", recipe_list, name="recipe_list"),
     path("recipe/<int:pk>/", recipe_detail, name="recipe_detail"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
